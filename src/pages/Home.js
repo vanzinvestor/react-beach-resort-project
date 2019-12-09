21a389f97264f9ef6d2f0455b6da6b046483fc65
Vanz Investor <vanzinvestor@gmail.com>
2019-12-09 20:39:28 +0700

    Banner Component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Hero from '../components/Hero';
+import Banner from '../components/Banner';
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <Hero>
+      <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
+        <Link to="/rooms" className="btn-primary">
+          our rooms
+        </Link>
+      </Banner>
+    </Hero>
+  );
 }
