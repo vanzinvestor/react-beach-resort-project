56f8608e5cae05110b33a9ab16e6cc79c1a5d75a
Vanz Investor <vanzinvestor@gmail.com>
2019-12-10 13:11:42 +0700

    Loading Component Setup

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import { RoomContext } from '../context';
+import Loading from './Loading';
+import Room from './Room';
 
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
     const { featuredRooms: rooms } = this.context;
     console.log(rooms);
-    return <div>from feature room</div>;
+    return (
+      <div>
+        from featured room
+        <Room />
+        <Loading />
+      </div>
+    );
   }
 }
