eb454f1d0bdf1be4d28c49a0a53e0c7cfa5c0f85
Vanz Investor <vanzinvestor@gmail.com>
2019-12-10 14:35:31 +0700

    Single Room Setup

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import defautBcg from '../images/room-1.jpeg';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { RoomContext } from '../context';
 
 export default class SingleRoom extends Component {
+  constructor(props) {
+    super(props);
+    // console.log(this.props);
+    this.state = { slug: this.props.match.params.slug, defautBcg };
+  }
+
+  static contextType = RoomContext;
+
+  componentDidMount() {}
+
   render() {
+    const { getRoom } = this.context;
+    const room = getRoom(this.state.slug);
+    console.log(room);
+
     return <div>hello from single room page</div>;
   }
 }
