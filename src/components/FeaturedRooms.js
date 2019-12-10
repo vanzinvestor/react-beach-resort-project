dade60ff5a5fd0d1aee5dd392920d8379eb99248
Vanz Investor <vanzinvestor@gmail.com>
2019-12-10 13:21:46 +0700

    Featured Rooms Component

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -2,18 +2,21 @@ import React, { Component } from 'react';
 import { RoomContext } from '../context';
 import Loading from './Loading';
 import Room from './Room';
+import Title from './Title';
 
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
-    const { featuredRooms: rooms } = this.context;
-    console.log(rooms);
+    let { loading, featuredRooms: rooms } = this.context;
+    rooms = rooms.map(room => <Room key={room.id} room={room} />);
+
     return (
-      <div>
-        from featured room
-        <Room />
-        <Loading />
-      </div>
+      <section className="featured-rooms">
+        <Title title="featured rooms" />
+        <div className="featured-rooms-center">
+          {loading ? <Loading /> : rooms}
+        </div>
+      </section>
     );
   }
 }
