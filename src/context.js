b8f5052ba94522977306be2248fe7cbdfbe5cd80
Vanz Investor <vanzinvestor@gmail.com>
2019-12-10 21:16:12 +0700

    Room Filter Setup

--- src/context.js
+++ src/context.js
@@ -9,17 +9,32 @@ class RoomProvider extends Component {
     sortedRooms: [],
     featuredRooms: [],
     loading: true,
+    type: 'all',
+    capacity: 1,
+    price: 0,
+    minPrice: 0,
+    maxPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false,
+    pets: false,
   };
 
   // getData
   componentDidMount() {
     let rooms = this.formatData(items);
     let featuredRooms = rooms.filter(room => room.featured === true);
+    let maxPrice = Math.max(...rooms.map(item => item.price));
+    let maxSize = Math.max(...rooms.map(item => item.size));
+
     this.setState({
       rooms,
       featuredRooms,
       sortedRooms: rooms,
       loading: false,
+      price: maxPrice,
+      maxPrice,
+      maxSize,
     });
   }
 
@@ -42,9 +57,26 @@ class RoomProvider extends Component {
     return room;
   };
 
+  handleChange = e => {
+    const type = e.target.type;
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log(type, name, value);
+  };
+
+  filterRooms = () => {
+    console.log('hello');
+  };
+
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
+      <RoomContext.Provider
+        value={{
+          ...this.state,
+          getRoom: this.getRoom,
+          handleChange: this.handleChange,
+        }}
+      >
         {this.props.children}
       </RoomContext.Provider>
     );
