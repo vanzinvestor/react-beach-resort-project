8edb26a86885936ff8484c4c35e280de0085e0f5
Vanz Investor <vanzinvestor@gmail.com>
2019-12-11 13:09:55 +0700

    Consume Content

---
+++ src/Contentful.js
@@ -0,0 +1,6 @@
+import { createClient } from 'contentful';
+
+export default createClient({
+  space: 'raqoxfod33k4',
+  accessToken: '_3oliEboXtZj02AH1MxuVVm7K7HcLGsGvg4ionUd2HU',
+});
--- src/context.js
+++ src/context.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import items from './data';
+// import items from './data';
+import Client from './Contentful';
 
 const RoomContext = React.createContext();
 
@@ -21,21 +22,36 @@ class RoomProvider extends Component {
   };
 
   // getData
-  componentDidMount() {
-    let rooms = this.formatData(items);
-    let featuredRooms = rooms.filter(room => room.featured === true);
-    let maxPrice = Math.max(...rooms.map(item => item.price));
-    let maxSize = Math.max(...rooms.map(item => item.size));
+  getData = async () => {
+    try {
+      let response = await Client.getEntries({
+        content_type: 'beachResortRoom',
+        order: 'sys.createdAt',
+        // order: 'fields.price',
+        // order: '-fields.price',
+      });
+      console.log(response.items);
+      let rooms = this.formatData(response.items);
+      let featuredRooms = rooms.filter(room => room.featured === true);
+      let maxPrice = Math.max(...rooms.map(item => item.price));
+      let maxSize = Math.max(...rooms.map(item => item.size));
 
-    this.setState({
-      rooms,
-      featuredRooms,
-      sortedRooms: rooms,
-      loading: false,
-      price: maxPrice,
-      maxPrice,
-      maxSize,
-    });
+      this.setState({
+        rooms,
+        featuredRooms,
+        sortedRooms: rooms,
+        loading: false,
+        price: maxPrice,
+        maxPrice,
+        maxSize,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  componentDidMount() {
+    this.getData();
   }
 
   formatData(items) {
